d361a9b0c0e14640bf9d318e473168ed271c770e
ClimentMiguel <mcliment0902@gmail.com>
2024-01-07 09:12:00 +0100

    First Commit
    Bottom text

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+
+st.title("Title")
+st.header("Header")
+st.subheader("Subheader")
+st.text("Text")
+st.image("BMI to avg sleep.png", caption="BMI to Average Sleep")
+with st.expander("Video sobre obesidad"):
+    st.video("https://www.youtube.com/watch?v=llJsK0pjM2s")
+c1, c2, c3, c4, c5 = st.columns(5)
+c1.button("Mi botón")
+c2.checkbox("Un Checkbox")
+c3.number_input("Input de número", value=5, min_value=0, max_value=10, step=1)
+test = c4.text_input("Input de texto")
+c5.metric("EL bicho", "#7")
+
+
+with st.empty():
+    st.write(test)
+
+
