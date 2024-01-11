d96dffa315bc6432a10b3fcb0a52f1c5326610c8
ClimentMiguel <mcliment0902@gmail.com>
2024-01-11 08:41:39 +0100

    TEST 11

--- pages/5_🫃_BMI.py
+++ pages/5_🫃_BMI.py
@@ -1,7 +1,6 @@
 import streamlit as st
 st.set_page_config(page_title="BMI")
 
-
 st.text("Text")
 st.image("BMI to avg sleep.png", caption="BMI to Average Sleep")
 with st.expander("Video sobre obesidad"):
