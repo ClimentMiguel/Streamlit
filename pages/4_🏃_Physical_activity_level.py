bd7763d6af197ff6e6279b2a3d186d102dadedc8
Tomás Travis Alonso Cremnitz <149080929+tomastravis@users.noreply.github.com>
2024-01-12 17:25:24 +0100

    Update 4_🏃_Physical_activity_level.py
    Datos por nivel de physical activity level

--- pages/4_🏃_Physical_activity_level.py
+++ pages/4_🏃_Physical_activity_level.py
@@ -6,7 +6,8 @@ import plotly.express as px  # interactive charts
 import streamlit as st  # 🎈 data web app development
 
 df = pd.read_csv('/Users/Tomasss/uni/23_24Master/Visualizacion_de_datos/Entrega/Sleep_health_and_lifestyle_dataset.csv')
-df
 
 # top-level filters
-Physical_activity_filter = st.selectbox("Select the physical activity level", pd.unique(df["Physical Activity Level"].sort_index("Physical Activity Level")))
+Physical_activity_filter = st.selectbox("Select the physical activity level", pd.unique(df["Physical Activity Level"]))
+df = df[df["Physical Activity Level"] == Physical_activity_filter]
+df
