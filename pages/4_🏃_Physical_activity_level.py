ee391d47244a976ca140acb7efb6434f1e3528f3
Tomás Travis Alonso Cremnitz <149080929+tomastravis@users.noreply.github.com>
2024-01-12 19:57:30 +0100

    Update 4_🏃_Physical_activity_level.py
    Intento 3

--- pages/4_🏃_Physical_activity_level.py
+++ pages/4_🏃_Physical_activity_level.py
@@ -8,6 +8,18 @@ import streamlit as st  # 🎈 data web app development
 df = pd.read_csv('/Users/Tomasss/uni/23_24Master/Visualizacion_de_datos/Entrega/Sleep_health_and_lifestyle_dataset.csv')
 
 # top-level filters
-Physical_activity_filter = st.selectbox("Select the physical activity level", pd.unique(df["Physical Activity Level"]))
-df = df[df["Physical Activity Level"] == Physical_activity_filter]
-df
+#Physical_activity_filter = st.selectbox("Select the physical activity level", pd.unique(df["Physical Activity Level"]))
+#df = df[df["Physical Activity Level"] == Physical_activity_filter]
+#df
+
+
+st.markdown("### First Chart")
+fig = px.density_heatmap(
+    data_frame=df, y="Physical Activity Level", x="Quality of Sleep"
+)
+st.write(fig)
+   
+
+st.markdown("### Second Chart")
+fig2 = px.histogram(data_frame=df, x="Daily Steps")
+st.write(fig2)
