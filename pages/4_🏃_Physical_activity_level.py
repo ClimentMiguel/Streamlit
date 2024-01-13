abfcea259fbbd0104d70308152a83d7dc9e77498
Tomás Travis Alonso Cremnitz <149080929+tomastravis@users.noreply.github.com>
2024-01-13 11:28:49 +0100

    Relación Physical Activity Level con Daily Steps

--- pages/4_🏃_Physical_activity_level.py
+++ pages/4_🏃_Physical_activity_level.py
@@ -4,6 +4,7 @@ import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
 import plotly.express as px  # interactive charts
 import streamlit as st  # 🎈 data web app development
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('/Users/Tomasss/uni/23_24Master/Visualizacion_de_datos/Entrega/Sleep_health_and_lifestyle_dataset.csv')
 
@@ -11,6 +12,8 @@ df = pd.read_csv('/Users/Tomasss/uni/23_24Master/Visualizacion_de_datos/Entrega/
 #Physical_activity_filter = st.selectbox("Select the physical activity level", pd.unique(df["Physical Activity Level"]))
 #df = df[df["Physical Activity Level"] == Physical_activity_filter]
 #df
+st.markdown('## Correlacion actividad fisica ~ pasos diarios')
+st.scatter_chart(data=df, x = 'Physical Activity Level', y = 'Daily Steps', color=None, size=None, width=0, height=0, use_container_width=True)
 
 
 st.markdown("### First Chart")
@@ -23,3 +26,4 @@ st.write(fig)
 st.markdown("### Second Chart")
 fig2 = px.histogram(data_frame=df, x="Daily Steps")
 st.write(fig2)
+
