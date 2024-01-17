2bf45969f32f57927fc643991ebe4da4f0651afa
ClimentMiguel <mcliment0902@gmail.com>
2024-01-17 07:35:58 +0100

    Added stuff+
    >added sleep disorder part for heart rate
    >Text to spanish

--- pages/7_🩸_Heart_rate_&_Blood_Pressure.py
+++ pages/7_🩸_Heart_rate_&_Blood_Pressure.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import statsmodels
+import plotly.graph_objects as go
+
 
 st.set_page_config(page_title="Heart rate & Blood Pressure")
 
@@ -14,10 +15,10 @@ def generate_graph_bp(df, type_1,type_2):
 
     return fig
 
-st.title("Effects of sleep on blood pressure & Heartrate")
+st.title("Efectos del sueño en la presión sanguínea y las pulsaciones cardiacas")
 
 
-st.header("Blood pressure")
+st.header("Presión sanguínea")
 
 bloodPressure = pd.DataFrame(file["Blood Pressure"])
 
@@ -25,8 +26,7 @@ bloodPressure[['Systolic', 'Diastolic']] = bloodPressure['Blood Pressure'].str.s
 bloodPressure = bloodPressure.drop("Blood Pressure", axis = 1)
 systolic = pd.DataFrame(bloodPressure["Systolic"])
 diastolic = pd.DataFrame(bloodPressure["Diastolic"])
-
-st.subheader("Sleep Duration")
+st.subheader("Duración del sueño")
 
 sleepDuration = pd.DataFrame(file["Sleep Duration"])
 
@@ -34,31 +34,31 @@ merged_sys = pd.concat([sleepDuration, systolic], axis=1)
 merged_dias = pd.concat([sleepDuration, diastolic], axis=1)
 
 c1, c2= st.columns(2)
-checkSys = c1.button("Systolic", key = "1")
-checkDias = c2.button("Diastolic", key = "2")
+checkSys = c1.button("Sistólica", key = "1")
+checkDias = c2.button("Diastólica", key = "2")
 
 if checkSys == True:
 
     fig = generate_graph_bp(merged_sys,"Sleep Duration","Systolic")
-    fig.update_layout(xaxis_title= "Sleep Duration (hours)", yaxis_title='Systolic Blood Pressure (mm Hg)',
-                  legend_title='Legend', autosize=False, width=800, height=500,showlegend=False, title='Effect of sleep duration on blood pressure')
+    fig.update_layout(xaxis_title= "Duración del sueño (horas)", yaxis_title='Presión sistólica (mm Hg)',
+                  legend_title='Legend', autosize=False, width=800, height=500,showlegend=False, title='Efectos de la duración del sueño en la presión sistólica')
     fig.update_traces(marker=dict(color='red'))
     st.plotly_chart(fig)
 elif checkDias == True:
 
     fig = generate_graph_bp(merged_dias,"Sleep Duration","Diastolic")
-    fig.update_layout(xaxis_title="Sleep Duration (hours)", yaxis_title='Diastolic Blood Pressure (mm Hg)',
-                  legend_title='Legend', autosize=False, width=800, height=500,showlegend=False, title='Effect of sleep duration on blood pressure')
+    fig.update_layout(xaxis_title="Duración del sueño (horas)", yaxis_title='Presión diastólica (mm Hg)',
+                  legend_title='Legend', autosize=False, width=800, height=500,showlegend=False, title='Efectos de la duración del sueño en la presión diastólica')
     fig.update_traces(marker=dict(color='green'))
     st.plotly_chart(fig)
 
-st.subheader("Sleep Quality")
+st.subheader("Calidad del sueño")
 
 sleepQuality = pd.DataFrame(file["Quality of Sleep"])
 
 c1, c2= st.columns(2)
-checkSysQ = c1.button("Systolic", key= "3")
-checkDiasQ = c2.button("Diastolic", key = "4")
+checkSysQ = c1.button("Sistólica", key= "3")
+checkDiasQ = c2.button("Diastólica", key = "4")
 
 merged_sysQ = pd.concat([sleepQuality, systolic], axis=1)
 merged_diasQ = pd.concat([sleepQuality, diastolic], axis=1)
@@ -66,40 +66,57 @@ merged_diasQ = pd.concat([sleepQuality, diastolic], axis=1)
 if checkSysQ == True:
 
     fig = generate_graph_bp(merged_sysQ,"Quality of Sleep","Systolic")
-    fig.update_layout(xaxis_title= "Sleep Quality", yaxis_title='Systolic Blood Pressure (mm Hg)',
-                  legend_title='Legend', autosize=False, width=800, height=500,showlegend=False, title='Effect of sleep quality on blood pressure')
+    fig.update_layout(xaxis_title= "Calidad del sueño", yaxis_title='Presión sistólica (mm Hg)',
+                  legend_title='Legend', autosize=False, width=800, height=500,showlegend=False, title='Efectos de la calidad del sueño en la presión sistólica')
     fig.update_traces(marker=dict(color='red'))
     st.plotly_chart(fig)
 elif checkDiasQ == True:
 
     fig = generate_graph_bp(merged_diasQ,"Quality of Sleep","Diastolic")
-    fig.update_layout(xaxis_title="Sleep Quality", yaxis_title='Diastolic Blood Pressure (mm Hg)',
-                  legend_title='Legend', autosize=False, width=800, height=500,showlegend=False, title='Effect of sleep quality on blood pressure')
+    fig.update_layout(xaxis_title="Calidad del sueño", yaxis_title='Presión diastólica (mm Hg)',
+                  legend_title='Legend', autosize=False, width=800, height=500,showlegend=False, title='Efectos de la calidad del sueño en la presión diastólica')
     fig.update_traces(marker=dict(color='green'))
     st.plotly_chart(fig)
 
-st.header("Heart rate")
+
+st.header("Pulsación cardiaca")
 
 heartRate = pd.DataFrame(file["Heart Rate"])
 
-st.subheader("Sleep Duration")
+st.subheader("Duración del sueño")
 
 merged_HR = pd.concat([sleepDuration,heartRate],axis = 1)
 
 fig = generate_graph_bp(merged_HR,"Sleep Duration", "Heart Rate")
-fig.update_layout(xaxis_title="Sleep Duration (hours)", yaxis_title='Heart Rate (bpm)',
-                  legend_title='Legend', autosize=False, width=800, height=500,showlegend=False, title = "Effects of duration of sleep on heart rate")
+fig.update_layout(xaxis_title="Duración del sueño (horas)", yaxis_title='Pulsación cardiaca (bpm)',
+                  legend_title='Legend', autosize=False, width=800, height=500,showlegend=False, title = "Efectos de la duración del sueño en las pulsaciones cardiacas")
 fig.update_traces(marker=dict(color='blue'))
 st.plotly_chart(fig)
 
-st.subheader("Sleep Quality")
+st.subheader("Calidad del sueño")
 
 merged_HRQ = pd.concat([sleepQuality, heartRate],axis = 1)
 
 fig = generate_graph_bp(merged_HRQ,"Quality of Sleep", "Heart Rate")
-fig.update_layout(xaxis_title="Sleep Quality", yaxis_title='Heart Rate (bpm)',
-                  legend_title='Legend', autosize=False, width=800, height=500,showlegend=False, title = "Effects of duration of sleep on heart rate")
+fig.update_layout(xaxis_title="Calidad del sueño", yaxis_title='Pulsación cardiaca (bpm)',
+                  legend_title='Legend', autosize=False, width=800, height=500,showlegend=False, title = "Efectos de la calidad del sueño en las pulsaciones cardiacas")
 fig.update_traces(marker=dict(color='purple'))
 st.plotly_chart(fig)
 
+st.subheader("Trastornos del sueño")
 
+file["Sleep Disorder"] = file["Sleep Disorder"].fillna(value="None")
+
+avgHR = [file[file["Sleep Disorder"]==el]["Heart Rate"].mean() for el in ["None","Sleep Apnea", "Insomnia"]]
+
+fig = go.Figure()
+fig.add_trace(go.Bar(
+    x=["None","Sleep Apnea","Insomnia"],
+    y=avgHR,
+    name='Primary Product',
+    marker_color='indianred')
+    )
+fig.update_xaxes(title = "Trastorno del sueño")
+fig.update_yaxes(title = "Pulsación cardiaca (bpm)")
+fig.update_layout(title = "Efectos de los trastornos de sueño en las pulsaciones cardiacas")
+st.plotly_chart(fig, use_container_width=True)
