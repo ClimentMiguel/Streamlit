6e515b5906737dd5ad7e9e3b53246078cfa35196
ClimentMiguel <mcliment0902@gmail.com>
2024-01-17 08:04:38 +0100

    Added content for conclusion
    >Yea I did hehe

--- pages/8_🥅_Conclusions.py
+++ pages/8_🥅_Conclusions.py
@@ -3,4 +3,19 @@ import streamlit as st
 st.set_page_config(page_title="Conclusions")
 
 st.header("Conclusión")
-st.image("./The Sleep study conclusions.jpeg")
+st.subheader("Qué factores son los más influyente para no padecer ninguna enfermedad relacionada con el sueño?")
+
+
+st.markdown("- Los hombres tienen una probabilidad menor a sufrir trastornos de sueño ")
+st.markdown("- Las edades más tempranas ")
+st.markdown("- Un IMC clasificado como normal")
+st.markdown("- Actividad física media ")
+st.markdown("- Nivel de estrés por debajo de 5")
+st.markdown("- Tipo de profesión")
+
+st.image("./The Sleep study conclusions.jpeg", width=400)
+
+
+
+
+
